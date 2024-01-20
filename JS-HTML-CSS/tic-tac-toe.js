f698efcfb1f1f83c364f0b983500df21819e6199
AbolfazlJahangir <jahangirabolfazl@gmail.com>
2024-01-20 22:21:39 +0330

    Change background picture

--- JS-HTML-CSS/tic-tac-toe.js
+++ JS-HTML-CSS/tic-tac-toe.js
@@ -19,6 +19,7 @@ function clearDisplay() {
     }
 
     for (let i = 1; i <= 9; i++) {
+        document.getElementById(i).style.backgroundColor = '#00ffaa';
         document.getElementById(i).innerHTML = "";
     }
     
@@ -64,27 +65,70 @@ function emptySpace() {
 function checkWinner() {
     for (let i = 0; i < 3; i++) {
         if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][2] != " ") {
+            if (i == 0) {
+                document.getElementById("1").style.backgroundColor = 'green';
+                document.getElementById("2").style.backgroundColor = 'green';
+                document.getElementById("3").style.backgroundColor = 'green';
+            }
+            else if (i == 1) {
+                document.getElementById("4").style.backgroundColor = 'green';
+                document.getElementById("5").style.backgroundColor = 'green';
+                document.getElementById("6").style.backgroundColor = 'green';
+            }
+            else if (i == 2) {
+                document.getElementById("7").style.backgroundColor = 'green';
+                document.getElementById("8").style.backgroundColor = 'green';
+                document.getElementById("9").style.backgroundColor = 'green';
+            }
             disableButton();
             return true;
         }
     }
     for (let j = 0; j < 3; j++) {
         if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[2][j] != " ") {
+            if (j == 0) {
+                document.getElementById("1").style.backgroundColor = 'green';
+                document.getElementById("4").style.backgroundColor = 'green';
+                document.getElementById("7").style.backgroundColor = 'green';
+            }
+            else if (j == 1) {
+                document.getElementById("2").style.backgroundColor = 'green';
+                document.getElementById("5").style.backgroundColor = 'green';
+                document.getElementById("8").style.backgroundColor = 'green';
+            }
+            else if (j == 2) {
+                document.getElementById("3").style.backgroundColor = 'green';
+                document.getElementById("6").style.backgroundColor = 'green';
+                document.getElementById("9").style.backgroundColor = 'green';
+            }
             disableButton();
             return true;
         }
     }
     if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] != " ") {
+        document.getElementById("1").style.backgroundColor = 'green';
+        document.getElementById("5").style.backgroundColor = 'green';
+        document.getElementById("9").style.backgroundColor = 'green';
         disableButton();
         return true;
     }
     if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[2][0] != " ") {
+        document.getElementById("3").style.backgroundColor = 'green';
+        document.getElementById("5").style.backgroundColor = 'green';
+        document.getElementById("7").style.backgroundColor = 'green';
         disableButton();
         return true;
     }
 
-    if (emptySpace()) return false;
-    else return "Tie";
+    if (emptySpace()) {
+        return false;
+    }
+    else {
+        for (let i = 1; i <= 9; i++) {
+            document.getElementById(i).style.backgroundColor = '#ffff00';
+        }
+        return "Tie";
+    }
 }
 
 function buttonBoard(x, y) {
